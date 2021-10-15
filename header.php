4e3ba0bf5a3de93e77adad8563455a06128422d8
guilhem kieny <guilhem.kieny@gmail.com>
2021-10-15 12:22:53 +0200

    homepage finish

---
+++ header.php
@@ -0,0 +1,32 @@
+<header id="main-header">
+    <div class="header-main-container">
+        <div class="header-logo-container">
+            <img src="./images/logo-or.png" alt="logo du site" class="header-logo">
+        </div>
+
+        <div class="header-title-container">
+            <h1>LEAGUE OF SKINS</h1>
+        </div>
+        
+        <div class="header-li-container">
+            <ul>
+                <li>
+                    <a href="">Se connecter</a>
+                </li>
+                <li>
+                    <a href="">Mon panier</a>
+                </li>
+            </ul>    
+        </div>
+    </div>
+    <nav class="nav-header">
+        <ul>
+            <li class="header-nav-li">
+                <a>Skins</a>
+            </li>
+            <li class="header-nav-li">
+                <a>Mes skins</a>
+            </li>
+        </ul>
+    </nav>
+</header>
--- main.php
+++ main.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="//db.onlinewebfonts.com/c/12420e8c141ca7c3dff41de2d59df13e?family=BeaufortforLOL-Bold" rel="stylesheet" type="text/css"/>
     <title>League of skins</title>
 </head>
 <body>
@@ -14,58 +15,93 @@
         ?>
         <section>
             <div class="main-title-container">
-                <h1>Liste des Skins</h1>
+                <h2>Liste des Skins</h2>
             </div>
                 
-            <div class="top-skins-list-container">
-                
+            <div class="top-skins-list-container">  
                 <input type="search" placeholder="Rechercher..." class="top-search">
+                <div class="dropdown">
+                    <span>Champions</span>
+                    <ul class="dropdown-content">
+                      <li><a href="">AATROX<a></li>  
+                      <li><a href="">AHRI</a></li> 
+                    </ul>
+                </div>
             </div>
             
         </section>
         <section id="all-skins-main-container">
             <div class="champion-skins-main-container">
                 <div class="champion-title-container">
-                     <h3>Aatrox</h3>
+                        <h3>AATROX</h3>
                 </div>
             
                 <div class="champion-skins-container">
-                    <div class="champion-skin-card">
-                        <img src="./images/Aatrox/Aatrox_justicier.jpg" alt="Aatrox justicier">
-                        <h4>Aatrox justicier</h4>
-                    </div>
-                    <div class="champion-skin-card">
-                        <img src="./images/Aatrox/Aatrox_mecha.jpg" alt="Mecha Aatrox">
-                        <h4>Mecha Aatrox</h4>
-                    </div>
-                    <div class="champion-skin-card">
-                        <img src="./images/Aatrox/Aatrox_pecheur.jpg" alt="Aatrox chasseur pecheur">
-                        <h4>Aatrox chasseur marin</h4>
-                    </div>
-                    <div class="champion-skin-card">
-                        <img src="./images/Aatrox/Aatrox_lune_de_sang.jpg" alt="Aatrox lune de sang">
-                        <h4>Aatrox lune de sang</h4>
-                    </div>
-                    <div class="champion-skin-card">
-                        <img src="./images/Aatrox/Aatrox_lune_de_sang_prestige.jpg" alt=" Aatrox lune de sang Edition Prestige">
-                        <h4>Aatrox lune de sang Edition Prestige</h4>
-                    </div>
-                    <div class="champion-skin-card">
-                        <img src="./images/Aatrox/aatrox_heros_de_guerre.jpg" alt="Aatrox héros de guerre">
-                        <h4>Aatrox héros de guerre</h4>
-                    </div>
+                    <a href="">
+                        <div class="champion-skin-card">
+                            <div class="champion-img-container">
+                                <img src="./images/Aatrox/Aatrox_justicier.jpg" alt="Aatrox justicier">
+                            </div>
+                            <h4>Aatrox justicier</h4>
+                        </div>
+                    </a>
+                    
+                    <a href="">
+                        <div class="champion-skin-card">
+                            <div class="champion-img-container">
+                                <img src="./images/Aatrox/Aatrox_mecha.jpg" alt="Mecha Aatrox">
+                            </div>
+                            <h4>Mecha Aatrox</h4>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="champion-skin-card">
+                            <div class="champion-img-container">
+                                <img src="./images/Aatrox/Aatrox_pecheur.jpg" alt="Aatrox chasseur pecheur">
+                            </div>
+                            <h4>Aatrox chasseur marin</h4>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="champion-skin-card">
+                            <div class="champion-img-container">
+                                <img src="./images/Aatrox/Aatrox_lune_de_sang.jpg" alt="Aatrox lune de sang">
+                            </div>
+                            <h4>Aatrox lune de sang</h4>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="champion-skin-card">
+                            <div class="champion-img-container">
+                                <img src="./images/Aatrox/Aatrox_lune_de_sang_prestige.jpg" alt=" Aatrox lune de sang Edition Prestige">
+                            </div>
+                            <h4>Aatrox lune de sang Edition Prestige</h4>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div class="champion-skin-card">
+                            <div class="champion-img-container">
+                                <img src="./images/Aatrox/aatrox_heros_de_guerre.jpg" alt="Aatrox héros de guerre">
+                            </div>
+                            <h4>Aatrox héros de guerre</h4>
+                        </div>
+                    </a>
                 </div>
                 
             </div>
             <div class="champion-skins-main-container">
                 <div class="champion-title-container">
-                  <h3>Ahri</h3>  
+                    <h3>Ahri</h3>  
                 </div>
                 <div class="champion-skins-container">
-                    <div class="champion-skin-card">
-                        <img src="" alt="">
-                        <h4>Ahri dynastique</h4>
-                    </div>
+                    <a href="">
+                        <div class="champion-skin-card">
+                            <div class="champion-img-container">
+                                <img src="" alt="">
+                            </div>
+                            <h4>Ahri dynastique</h4>
+                        </div>
+                    </a>
                 </div>
             </div>
         </section>
